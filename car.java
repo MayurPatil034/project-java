494a91efd22f52faa164a56d6db54ec37d1d2e92
Mayur Patil <mspatil.work@outlook.com>
2025-01-11 22:05:42 +0530

    initial commit

--- car.java
+++ car.java
@@ -1,5 +1,10 @@
 public class car {
     public static void main(String[] args) {
-        System.out.println("hello this is test code for git and github");
+        System.out.println("hello this is test code for git and  github");
+        int a = 1;
+        while(a<50){
+            System.out.println(a);
+            a++;
+        }
     }
 }
